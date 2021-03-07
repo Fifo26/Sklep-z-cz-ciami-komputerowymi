d673b40d84ea57f7f24b5e8657edbaf48e5cdd71
KacoPC <kacperrtv@gmail.com>
2021-03-07 23:51:48 +0100

    add navibar

---
+++ navibar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+            <div class="container"><a class="navbar-brand" href="#">Sklep Fifa</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav mr-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">główna</a></li>
+                    </ul><span class="navbar-text actions"> <a href="#" class="login">Zaloguj</a><a class="btn btn-light action-button" role="button" href="#">Zarejestruj</a></span></div>
+            </div>
+        </nav>
